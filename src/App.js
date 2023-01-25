2bbc3f5c0d70a5c84a3446ff7a2a62c436f40772
aminur <mohammadnoorcse#@gmail.com>
2023-01-26 00:42:28 +0600

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import {Routes,Route,useLocation} from "react-router-dom"
+import Home from "./containers/home/Home"
+import About from './containers/about/About';
+import Resume from "./containers/resume/Resume"
+import Contact from './containers/contact/Contact';
+import Skills from "./containers/skills/Skills";
+import Portfolio from "./containers/portfolio/Portfolio"
+import Navbar from './components/navbar/Navbar';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+import particles from './utils/particles';
 function App() {
+   const location = useLocation();
+   console.log(location);
+
+  const handleInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const renderParticleJsInHomePage = location.pathname === "/";
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {renderParticleJsInHomePage &&
+      <Particles id="particles" options={particles} init={handleInit}  />
+      
+      }
+     
+      <Navbar/>
+      <div className="App__main-page-content">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/resume' element={<Resume/>}/>
+        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      </div>
+      </div>
+    
+    
+ 
   );
 }
 
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Development",
+        subTitle: "KKhudeGhore",
+        description:
+          " User Experience, Visual Design",
+      },
+      // {
+      //   title: "Senior Consultant",
+      //   subTitle: "London, UK",
+      //   description:
+      //     "Web development, Databases, Backend developement",
+      // },
+      // {
+      //   title: "Project Manager",
+      //   subTitle: "Bangalore, INDIA",
+      //   description:
+      //     "Creative Direction Project Management, Team Leading",
+      // },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Dewpara high school",
+        description:
+          "GPA 5",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Uttara Residentail School and College",
+        description:
+          "GPA 4.58 in Science",
+      },
+      {
+        title: "BSC",
+        subTitle: "Daffodils International University",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
